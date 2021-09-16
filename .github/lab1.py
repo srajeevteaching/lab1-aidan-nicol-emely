f2ca85d4f7cd5775acd2b42321555dec8bfc08b9
aidan <ajshaughnessy@loyola.edu>
2021-09-16 10:35:42 -0400

    lab1

---
+++ .github/lab1.py
@@ -0,0 +1,17 @@
+import math
+
+# Programmers: Aidan, Nicol, Emely
+# Course: CS151, Dr. Rajeev
+# Date: 9/16/2021
+# Lab Number: 1
+# Program Inputs: ML
+# Program Outputs: TBS & TSP
+
+startMl = input("Input ML:")
+
+convertTsp = float(startMl) / 4.929
+convertTbs = float(startMl) / 14.787
+
+print("Teaspoon:", convertTsp)
+print("Tablespoon:", convertTbs)
+
